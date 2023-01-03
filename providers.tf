28657aca7961164d9b1cdc0fe809679ace8a2d91
Miran Muthanna <43168423+miran93@users.noreply.github.com>
2023-01-03 10:10:12 +0530

    Providers

--- providers.tf
+++ providers.tf
@@ -1,4 +1,16 @@
-provider "aws" {
-    region = "${var.region}"
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
+    }
+  }
 }
 
+provider "aws" {
+    region = "${var.region}"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+}
